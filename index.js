e3c7c58ce8a4a00b20ed74689eb2c944358f7870
Aeciopereira <aeciojunior9978087732016@gmail.com>
2022-09-16 21:31:09 -0300

    LAÇOS DE REPETIÇÃO JS

---
+++ index.js
@@ -0,0 +1,26 @@
+//laços de repetição while
+  
+
+ var repeat= 1;
+ while(repeat < 10 ){
+  console.log(repeat);
+
+
+  repeat++;
+ }
+ 
+//laços de repetiçãp FOR  
+
+
+
+for( var junp =0; junp <= 10; junp++){
+ console.log(junp);
+}
+
+
+
+//laços de repetição DO -->
+
+for (var i =0; i< 10; i++){
+  console.log(i);
+}
